27528baccf4b6c817a7d12029f65bcd737e94674
Shoota Kumano <shoota.dev@gmail.com>
2015-01-27 22:45:19 +0900

    Initial Commit

---
+++ actionsheet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  actionsheet
+//
+//  Created by 熊野修太 on 2015/01/27.
+//  Copyright (c) 2015年 anaguma.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ actionsheet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  actionsheet
+//
+//  Created by 熊野修太 on 2015/01/27.
+//  Copyright (c) 2015年 anaguma.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
